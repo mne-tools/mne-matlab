a087800fbcb5376598a613fe8f997d0e1d6d4709
Christoph Dinh <chdinh@nmr.mgh.harvard.edu>
2012-11-17 15:11:37 -0500

    Initial real-time commit

---
+++ matlab/mne_ex_rt.m
@@ -0,0 +1,75 @@
+function [info] = mne_ex_rt(mne_rt_server_ip, mne_rt_server_commandPort, mne_rt_server_fiffDataPort)
+%
+%   An example of a mne_rt_server real-time connection
+%
+%   function mne_ex_rt(mne_rt_server_ip, mne_rt_server_commandPort ,mne_rt_server_fiffDataPort)
+%
+%   mne_rt_server_ip           - IP of the running mne_rt_server
+%   mne_rt_server_commandPort  - Command port of the mne_rt_server
+%   mne_rt_server_fiffDataPort - Fiff data port of the mne_rt_server
+%
+%   Returns the measurement info
+%   
+%
+
+%
+%   Author : Christoph Dinh, Matti Hamalainen, MGH Martinos Center
+%   License : BSD 3-clause
+%
+
+if nargin ~= 3
+    error(me,'Incorrect number of arguments');
+end
+
+%% add dynamic java path
+javaaddpath(fileparts(mfilename('fullpath')));
+
+%% create command client
+t_cmdClient = mne_rt_cmd_client(mne_rt_server_ip, mne_rt_server_commandPort);
+
+%% create data client
+t_dataClient = mne_rt_data_client(mne_rt_server_ip, mne_rt_server_fiffDataPort);
+
+%% set data client alias -> for convinience (optional)
+t_dataClient.setClientAlias('mne_ex_matlab'); % used in option 2 later on
+
+%% example commands
+t_helpInfo = t_cmdClient.sendCommand('help');
+fprintf('### Help ###\n%s',t_helpInfo);
+t_clistInfo = t_cmdClient.sendCommand('clist');
+fprintf('### Client List ###\n%s',t_clistInfo);
+t_conInfo = t_cmdClient.sendCommand('conlist');
+fprintf('### Connector List ###\n%s',t_conInfo);
+
+%% read meas info
+% Option 1
+t_aliasOrId = t_dataClient.getClientId();
+% Option 2
+%t_aliasOrId = 'mne_ex_matlab';
+t_cmdClient.requestMeasInfo(t_aliasOrId);
+t_measInfo = t_dataClient.readInfo();
+
+%% start measurement
+t_cmdClient.requestMeas(t_aliasOrId);
+
+figure;
+hold on;
+h_old=plot(0,0);
+while (true)
+    fprintf('read buffer...');
+    t_matRawBuffer = t_dataClient.readRawBuffer(t_measInfo.nchan);
+    %
+    %   You can add your own miracle here
+    %
+    fprintf('(%d channels x %d samples) [done]\r\n', size(t_matRawBuffer,1), size(t_matRawBuffer,2));
+    h = plot(t_matRawBuffer');
+    delete(h_old);
+    h_old = h;
+    drawnow;
+end
+
+%% close the sockets
+t_cmdClient.close();
+t_dataClient.close();
+
+end
