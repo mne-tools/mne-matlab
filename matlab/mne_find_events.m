7b58bb8329d9b0b0ac17ba3997b8e836d5a7b86f
zuxfoucault <zuxfoucault@gmail.com>
2017-09-18 22:44:20 +0800

    fix indentation

--- matlab/mne_find_events.m
+++ matlab/mne_find_events.m
@@ -1,33 +1,33 @@
 function [eventlist] = mne_find_events(fname, stim_channel, consecutive, output)
-%	
-%	
-%	[eventlist] = mne_find_events(fname, stim_channel, consecutive, output)
-%	
-%	Find event from raw file
-%	
-%	fname        - .fiff raw data file name
-%	stim_channel - the channel that record event
-%	consecutive  - bool | 'increasing'
-%				   If True, consider instances where the value of the events
-%				   channel changes without first returning to zero as multiple
-%				   events. If False, report only instances where the value of the
-%				   events channel changes from/to zero. If 'increasing', report
-%				   adjacent events only when the second event code is greater than
-%				   the first.
-%	output       - 'onset' | 'offset' | 'step'
-%				   Whether to report when events start, when events end, or both.
-%	
-%	eventlist    - size = (n_events, 3)
-%	    		   The first column contains the event time in samples and the third
-%	    		   column contains the event id. If output = 'onset' or 'step', the
-%	    		   second column contains the value of the stim channel immediately
-%	    		   before the event/step. For output = 'offset', the second column
-%	    		   contains the value of the stim channel after the event offset.
-%	
-%	Authors: Fu-Te Wong (zuxfoucault@gmail.com),
-%			 Chien-Chung Chen / Visual Neuroscience Lab, National Taiwan University
-%	Version 1.0 2017/9/17
-%	License: BSD (3-clause)
+%   
+%   
+%   [eventlist] = mne_find_events(fname, stim_channel, consecutive, output)
+%   
+%   Find event from raw file
+%   
+%   fname        - .fiff raw data file name
+%   stim_channel - the channel that record event
+%   consecutive  - bool | 'increasing'
+%                  If True, consider instances where the value of the events
+%                  channel changes without first returning to zero as multiple
+%                  events. If False, report only instances where the value of the
+%                  events channel changes from/to zero. If 'increasing', report
+%                  adjacent events only when the second event code is greater than
+%                  the first.
+%   output       - 'onset' | 'offset' | 'step'
+%                  Whether to report when events start, when events end, or both.
+%   
+%   eventlist    - size = (n_events, 3)
+%                  The first column contains the event time in samples and the third
+%                  column contains the event id. If output = 'onset' or 'step', the
+%                  second column contains the value of the stim channel immediately
+%                  before the event/step. For output = 'offset', the second column
+%                  contains the value of the stim channel after the event offset.
+%   
+%   Authors: Fu-Te Wong (zuxfoucault@gmail.com),
+%            Chien-Chung Chen / Visual Neuroscience Lab, National Taiwan University
+%   Version 1.0 2017/9/17
+%   License: BSD (3-clause)
 
 	raw = fiff_setup_read_raw(fname);
 	[data, times] = fiff_read_raw_segment(raw);
