3472e630fc4a6c9b1f5cad9e8cd40449897bd77f
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2023-11-28 14:49:34 +0100

    ENH - added unit test files

--- matlab/test/test_fiff_read_tag.m
+++ matlab/test/test_fiff_read_tag.m
@@ -4,8 +4,46 @@ function test_suite=test_fiff_read_tag
     initTestSuite;
 
 function test_fiff_read_tag_()
-% Test fiff_read_tag.m
-%
-% This is a placeholder function, referencing the below test function,
-% which should fail if fiff_read_tag does not work properly
-test_fiff_open
+% Test fiff_open.m
+
+if ispc
+  % I don't know what happens with the unit testing actually, if the
+  % function returns without actually testing anything
+  return;
+end
+
+f = which('test_fiff_define_constants.m');
+[p,f,e] = fileparts(f);
+
+% load the reference trees
+load(fullfile(p, 'data', 'test_fiftrees_ref.mat'));
+
+fname = fullfile(p, 'data', 'test_raw.fif');
+[fid, tree, dir] = fiff_open(fname);
+fclose(fid);
+assert(isequal(tree, trees.raw));
+
+fname = fullfile(p, 'data', 'test_long_raw.fif');
+[fid, tree, dir] = fiff_open(fname);
+fclose(fid);
+assert(isequal(tree, trees.longraw));
+
+fname = fullfile(p, 'data', 'test_raw-eve.fif');
+[fid, tree, dir] = fiff_open(fname);
+fclose(fid);
+assert(isequal(tree, trees.raweve));
+
+fname = fullfile(p, 'data', 'test-eve.fif');
+[fid, tree, dir] = fiff_open(fname);
+fclose(fid);
+assert(isequal(tree, trees.eve));
+
+fname = fullfile(p, 'data', 'test-ave.fif');
+[fid, tree, dir] = fiff_open(fname);
+fclose(fid);
+assert(isequal(tree, trees.ave));
+
+fname = fullfile(p, 'data', 'test-cov.fif');
+[fid, tree, dir] = fiff_open(fname);
+fclose(fid);
+assert(isequal(tree, trees.cov));
