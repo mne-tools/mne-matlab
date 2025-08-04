9a7990b19e3361731e937dcd9b839fcae88b6adc
neurosignal <amitjaiswal.elect@gmail.com>
2025-08-04 11:46:15 +0300

    formatted similar to other test files

--- matlab/test/test_read_string_digitization.m
+++ matlab/test/test_read_string_digitization.m
@@ -1,9 +1,16 @@
+function test_suite=test_read_string_digitization
+    try test_functions=localfunctions(); catch
+    end
+    initTestSuite;
 
-%% FIFF file name
-% fname = 'data/old_megin_data_raw_5s.fif'; % old file
-fname = 'data/raw_data_tsss_mc.fif'; % new file
+function test_read_string_digitization_()
 
-%% reading info with mne
+p = fileparts(mfilename('fullpath'));
+% % FIFF file name
+fname = fullfile(p, 'data', 'string_digi_test_data_1s.fif'); % a MEGIN data file with string type digitization
+
+% % reading info with mne-matlab
+% cd ..
 [fid, tree, dir] = fiff_open(fname);
 [info, meas] = fiff_read_meas_info(fid, tree);
 disp(info.dig)
